2d9d4864c7e914ef366620bffbcd0717aced1c1c
Elizabeth Ter Sahakyan <30808123+elizabethts@users.noreply.github.com>
2019-08-14 08:12:25 -0700

    lambdata-module

---
+++ __init__.py
@@ -0,0 +1,14 @@
+name = "lambdata"
+
+
+myName = 'Elizabeth Ter Sahakyan'
+
+new_list = [1,2,3,4,5,6]
+
+def checknull(df):
+	return df.isnull().sum()
+
+sample_df = [ [1,2,3],
+	      [4,5,6],
+	      [7,8,9] ]
+
